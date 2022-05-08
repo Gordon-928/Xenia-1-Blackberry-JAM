dd1066cab376af1d529ebdc6139cac103824ce6a
Gordon-928 <94927083+Gordon-928@users.noreply.github.com>
2022-05-08 10:31:41 -0700

    Adding De-bounce Feature in Arduino Code
    1. Added debounce feature in the code to prevent noise from IMU
    2. Updated comments in the code
    
    Note:
    1. Testing of the code is performed with a Arduino UNO with a circuit identical to actual circuit
    2. Testing is performed with serial monitor, code inside function is_1G() is replaced with Serial input

--- main/main.ino
+++ main/main.ino
@@ -28,14 +28,14 @@
 
 MMA8452Q accel;
 bool triggered = 0;
-unsigned long previousTime;
-unsigned long currentTime;
-const float cutoff = 0.25; //squared
+unsigned long previousTime = 0;
+unsigned long timer = 0;
+const float cutoff = 0.81; //0.9g squared 
+const float debounce = 200; //debounce time 200ms
 
 float x,y,z;
 
 int status = 1; //variable created for Serial test
-
 
 
 void setup(){
@@ -59,77 +59,83 @@ void setup(){
   Wire.begin();
   
   if (accel.begin()){
-    /*
+    //onboard LED blink sequence
     for (int i = 0; i < 3; i++){
       digitalWrite(LED, HIGH);
       delay(500);
       digitalWrite(LED, LOW);
       delay(200);
-    } */    
-  } else {/*
+    }    
+  } else {
       while(1){
           digitalWrite(LED, HIGH);
-      }*/
+      }
   }
 
     accel.active();
 
     if (accel.isActive()){
       
-      /* flashing Arduino LED to indicate IMU status
+      //flashing Arduino LED to indicate IMU status
       for (int i = 0; i < 5; i++){
         digitalWrite(LED, HIGH);
         delay(200);
         digitalWrite(LED, LOW);
         delay(500);
       } 
-      */
      Serial.println("MMA8452Q is active");  
     }
-
-  
 }
 
 void loop(){
-
-  while(!is_1G()){ //while not 1G (micro g)
-    digitalWrite(STBY, HIGH); //STBY (H)
-    digitalWrite(Motor_enable, HIGH); //PWMA (H)
-    digitalWrite(Motor,HIGH); //turn on motor -> AIN1 (H)
-    triggered = 1;
+  previousTime = millis(); //time stamp for next event 
+  Serial.println(millis() - previousTime);
+  
+  while(!is_1G()){
+    if((millis() - previousTime)>= debounce){//signal debounce
+      //while not 1G (micro g)
+      Serial.println(millis() - previousTime);
+      digitalWrite(STBY, HIGH); //STBY (H)
+      digitalWrite(Motor_enable, HIGH); //PWMA (H)
+      digitalWrite(Motor,HIGH); //turn on motor -> AIN1 (H)
+      triggered = 1;
+     }
   }
+  
   digitalWrite(Motor_enable,LOW); //turn off motor -> AIN1 (L)
+  previousTime = millis(); 
 
-  if(is_1G() && triggered ){
-    currentTime = millis();
-    previousTime = currentTime;
-    
-    while(((currentTime - previousTime)<= 2000) && is_1G()){
-      Serial.println(currentTime - previousTime);
-      digitalWrite(Pumps_enable, HIGH); //PWMB (H)
-      digitalWrite(Pumps, HIGH); //turn on motor -> BIN1 (H)
-      currentTime = millis();
-    }
-    digitalWrite(Pumps_enable, LOW); //turn off motor -> BIN1 (L)
-    triggered = 0;
-  }
-   
+  while(is_1G() && triggered){
+    if((millis() - previousTime)>= debounce){
+      timer = millis(); //time stamp
+      while(((millis() - timer)<= 2000) && is_1G()){//2 sec timer
+        Serial.println(millis() - timer);
+        digitalWrite(Pumps_enable, HIGH); //PWMB (H)
+        digitalWrite(Pumps, HIGH); //turn on motor -> BIN1 (H)
+        }
+      digitalWrite(Pumps_enable, LOW); //turn off motor -> BIN1 (L)
+      triggered = 0;
+      }
+   }
 }
 
 bool is_1G(){
-  /*
+  /* getting values from IMU
   x = accel.getCalculatedX();
   y = accel.getCalculatedY();
   z = accel.getCalculatedZ();
 
-  if((x*x+y*y+z*z) >= 1.0){
+  if((x*x+y*y+z*z) >= cutoff){
     return 1;
   }else {
     return 0;
   }
   */
+  
+  
   if(Serial.available() > 0){
     status = Serial.read() - '0'; 
+    Serial.print("status:");
     Serial.println(status);
   }
 
@@ -141,21 +147,6 @@ bool is_1G(){
     return 0;
   }
 }
-
-/*
-bool is_microG(){
-  x = accel.getCalculatedX();
-  y = accel.getCalculatedY();
-  z = accel.getCalculatedZ();
-
-  if((x*x+y*y+z*z) <= cutoff){
-    triggered = 1;
-    return 1;
-  }else{
-    return 0;
-    }
-}
-*/
 
 
 
